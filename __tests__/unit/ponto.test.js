17e17d2f00a7e877351f2d6213d6040e5875d3c9
Leonardo Guilen <leonardoguilen1@gmail.com>
2019-12-15 11:49:06 -0300

    Criado testes de unidade usando jest e alterações no package.json nos scripts para rodar em diferentes ambientes

---
+++ __tests__/unit/ponto.test.js
@@ -0,0 +1,47 @@
+// Index de eventos
+// entrada: 3, saida_almoco: 4, entrada_almoco: 5, saida: 6
+
+describe('Events', () => {
+    var getBatchID = require('../../src/utils.js');
+    const initialBatchID = "RXC2"; // X == Número da linha
+
+    it('Return cell BatchID valid for event type = Entrance', () => {
+        const event = "entrada";
+        const expectedBatchID = "RXC3";
+
+        var resultedBatchID = getBatchID(initialBatchID,event);
+
+        expect(expectedBatchID)
+        .toBe(resultedBatchID);
+    });
+
+    it('Return cell BatchID valid for event type = Lunch out', () => {
+        const event = "saida_almoco";
+        const expectedBatchID = "RXC4";
+                
+        var resultedBatchID = getBatchID(initialBatchID,event);
+
+        expect(expectedBatchID)
+        .toBe(resultedBatchID);
+    });
+
+    it('Return cell BatchID valid for event type = Lunch entrance', () => {
+        const event = "entrada_almoco";
+        const expectedBatchID = "RXC5";
+        
+        var resultedBatchID = getBatchID(initialBatchID,event);
+        
+        expect(expectedBatchID)
+        .toBe(resultedBatchID);
+    });
+
+    it('Return cell BatchID valid for event type = Exit', () => {
+        const event = "saida";
+        const expectedBatchID = "RXC6";
+        
+        var resultedBatchID = getBatchID(initialBatchID,event);
+        
+        expect(expectedBatchID)
+        .toBe(resultedBatchID);
+    });
+});
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+module.exports = (id_cell,event) => {
+    const index_eventos = { entrada: 3, saida_almoco: 4, entrada_almoco: 5, saida: 6 };
+    
+    switch (id_cell,event) {
+        case "entrada": {
+            id_cell = id_cell.replace("C2", "C" + index_eventos.entrada);
+        } 
+            break;
+        case "saida_almoco": {
+            id_cell = id_cell.replace("C2", "C" + index_eventos.saida_almoco);
+        } 
+            break;
+        case "entrada_almoco": {
+            id_cell = id_cell.replace("C2", "C" + index_eventos.entrada_almoco);
+        } 
+            break;
+        case "saida": {
+            id_cell = id_cell.replace("C2", "C" + index_eventos.saida);
+        } 
+            break;
+        default: {
+            id_cell = "Event not valid";
+        }
+    }
+
+    return id_cell;
+};
