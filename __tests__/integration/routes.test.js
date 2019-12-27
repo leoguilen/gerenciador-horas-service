1955c3b335441975927ebf7fdea43f7d8cbc2fb8
Leonardo Guilen <leonardoguilen1@gmail.com>
2019-12-27 14:50:52 -0300

    Melhoria nos teste de integração que agora realiza teste nas rotas do serviço, adicionado scripts para outras informações dos testes e exportado o app

--- __tests__/integration/routes.test.js
+++ __tests__/integration/routes.test.js
@@ -1,14 +1,45 @@
-const request = require('supertest');
-const app = require('../../server.js');
+const app = require('../../server')
+const supertest = require('supertest')
+const request = supertest(app)
 
 describe("Endpoints", () => {
-    it("should return spreadsheet info", async () => {
-        const res = await request(app)
-            .get("/spreadsheet/info");
-    }); 
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("Title");
-    expect(res.body).toHaveProperty("RowsCount");
-    expect(res.body).toHaveProperty("ColumnsCount");
-});
+    it('Has App Defined', () => {
+        expect(app).toBeDefined()
+    })
+
+    describe("Routes", () => {
+        
+        describe("GET Routes", () => {
+            it("should return spreadsheet info", async done => {
+                const res = await request.get('/api/spreadsheet/info')
+    
+                expect(res.status).toBe(200)
+                expect(res.body).toHaveProperty("Title")
+                expect(res.body).toHaveProperty("RowsCount")
+                expect(res.body).toHaveProperty("ColumnsCount")
+                
+                done()
+            })
+    
+            it("should return spreadsheet update info", async done => {
+                const res = await request.get('/api/spreadsheet/info/modifyInfo')
+    
+                expect(res.status).toBe(200)
+                expect(res.body).toHaveProperty("LastUpdate")
+                expect(res.body).toHaveProperty("AuthorName")
+                expect(res.body).toHaveProperty("AuthorEmail")
+    
+                done();
+            })
+        })
+
+        describe('POST Routes', () => {
+            // Rotas que utilizam o metodo POST
+        })
+
+        describe('DELETE Routes', () => {
+            // Rotas que utilizam o metodo DELETE
+        })
+    })
+})
