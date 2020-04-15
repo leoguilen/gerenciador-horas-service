a6e8decbc365cf57987a13fae8e0f4cb133615d2
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-04-14 23:08:33 -0300

    add procfile para startup do servico no heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-require('dotenv/config');
+const express = require("express");
+const cors = require("cors");
+require("dotenv/config");
 
 const _port = process.env.HOST_PORT;
 const _ip = process.env.HOST_IP;
@@ -7,9 +8,10 @@ const _ip = process.env.HOST_IP;
 // Starting App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
-app.listen(_port,_ip, () => console.log("Server is running..."));
+app.listen(_port, _ip, () => console.log("Server is running..."));
 
 module.exports = app;
